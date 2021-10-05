abe819031b7761fc5aa075d0d4350951c20c5229
johnmekus <mekus@bc.edu>
2021-10-04 22:04:56 -0400

    6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by John Mekus on 10/4/21.
+//
+
+import Foundation
+
+struct WeatherLocation
+{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
