ef809f98a4ea29d2c443b8bac6f64cb91086350a
johnmekus <mekus@bc.edu>
2021-10-24 21:28:25 -0400

    6.17

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -23,6 +23,7 @@ class LocationDetailViewController: UIViewController
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var weatherDetail: WeatherDetail!
     var locationIndex = 0
@@ -34,6 +35,8 @@ class LocationDetailViewController: UIViewController
         clearUserInterface()
         tableView.delegate = self
         tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         updateUserInterface()
     }
     
@@ -69,6 +72,7 @@ class LocationDetailViewController: UIViewController
                 self.summaryLabel.text = self.weatherDetail.summary
                 self.imageView.image = UIImage(named: self.weatherDetail.dayIcon)
                 self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         }
     }
@@ -120,5 +124,21 @@ extension LocationDetailViewController: UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
         return 80
+    }
+}
+
+extension LocationDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate
+{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return weatherDetail.hourlyWeatherData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath) as! HourlyCollectionViewCell
+        hourlyCell.hourlyWeather = weatherDetail.hourlyWeatherData[indexPath.row]
+        return hourlyCell
     }
 }
